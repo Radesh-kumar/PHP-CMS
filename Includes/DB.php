57fd33c99362aedecd6d4259da7141716f09514c
Radesh-kumar <imradesh@gmail.com>
2020-07-14 02:26:48 +0530

    1st commit

---
+++ Includes/DB.php
@@ -0,0 +1,5 @@
+<?php
+$DSN='mysql:host=us-cdbr-east-02.cleardb.com;port=3306;dbname=heroku_a5ff6984e7bd9fd';
+$ConnectingDB = new PDO($DSN,'b7f874d5e366ad','f2f44323');
+?>
+<!-- *** DONT LEAVE FUKIN SPACE IN CONNECTING ASSOLE *** -->
